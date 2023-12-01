1eb847bbd1804732ae61fd9eb5495b70a6678130
Rajan Kumar <raorajan9576@gmail.com>
2023-12-01 15:10:27 +0530

    bcrypt deleted

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+
 const errorHandler = require("./middlewares/error.middleware.js");
 const bookRouter = require("./routes/Book.route.js");
 const userRouter = require("./routes/User.route.js");
 const Connection = require("./config/db.js");
+
 const app = express();
 
 app.use(express.json());
@@ -24,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
   });
-  
+
 } else {
   app.get("/", (req, res) => {
     res.send("Server is Running! 🚀");
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const PORT = process.env.PORT || 4000;
 
-// UncaughtException Error
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   process.exit(1);
@@ -11,7 +10,6 @@ const server = app.listen(PORT, () => {
   console.log(`Server running on PORT http://localhost:${PORT}`);
 });
 
-// Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   server.close(() => {
