70af39a54153b5aa27b16051d7f04b223884760e
Rajan Kumar <raorajan9576@gmail.com>
2023-12-01 10:22:21 +0530

    Get Book By Id api Created

--- controllers/Book.controller.js
+++ controllers/Book.controller.js
@@ -16,6 +16,21 @@ const createBook = async (req, res, next) => {
   }
 };
 
+
+const getBookById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id);
+        if (!book) {
+            return res.status(404).json({ error: 'Book not found' });
+        }
+        res.status(200).json({ book });
+    } catch (error) {
+        next(error);
+    }
+};
+
 module.exports = {
   createBook,
+  getBookById,
 };
--- routes/Book.route.js
+++ routes/Book.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bookRouter = express.Router();
-const {createBook} = require('../controllers/Book.controller.js');
+const {createBook, getBookById} = require('../controllers/Book.controller.js');
 
 bookRouter.post('/create', createBook);
+bookRouter.get('/getById/:id',getBookById);
 
 module.exports = bookRouter;
