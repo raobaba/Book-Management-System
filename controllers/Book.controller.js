6fac05c0b9fa04411b0de9960c85f96ba49ccc41
Rajan Kumar <raorajan9576@gmail.com>
2023-12-01 10:25:16 +0530

    added status code and success message in response

--- controllers/Book.controller.js
+++ controllers/Book.controller.js
@@ -4,30 +4,33 @@ const createBook = async (req, res, next) => {
   try {
     const newBook = new Book(req.body);
     const savedBook = await newBook.save();
-    res
-      .status(201)
-      .json({
-        status: 201,
-        message: "Book created successfully",
-        book: savedBook,
-      });
+    res.status(201).json({
+      status: 201,
+      message: "Book created successfully",
+      book: savedBook,
+    });
   } catch (error) {
     next(error);
   }
 };
 
-
 const getBookById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const book = await Book.findById(id);
-        if (!book) {
-            return res.status(404).json({ error: 'Book not found' });
-        }
-        res.status(200).json({ book });
-    } catch (error) {
-        next(error);
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
     }
+    res
+      .status(200)
+      .json({
+        status: 201,
+        message: "Book retrieve successfully",
+        book
+      });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
